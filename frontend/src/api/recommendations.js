b7710e8a8f4455e416eb5e38219febf99068bf6e
Ye Fei Fan <1160907619@qq.com>
2025-12-24 13:42:26 +0800

    Revert "Refactor frontend: add router & Pinia, movie library, card-based recommendations UI"

--- frontend/src/api/recommendations.js
+++
@@ -1,8 +0,0 @@
-import http from './http';
-
-export const fetchRecommendations = async ({ userId, strategy, size }) => {
-  const response = await http.get('/recommendations', {
-    params: { userId, strategy, size }
-  });
-  return response.data;
-};
